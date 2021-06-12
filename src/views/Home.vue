636e200d9079b557e0aa1e3660bc81ef3f9bce75
furkan ceylan <ceylan.furkan@outlook.com>
2021-06-12 03:57:29 +0300

    task board and tasks component added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="header">
+      <TheHeader />
+    </div>
+    <div class="tasks-header">
+      <h1>Task Board</h1>
+      <div class="select-tasks">
+        <h4>All Tasks</h4>
+        <button class="material-icons">keyboard_arrow_down</button>
+      </div>
+    </div>
+    <Tasks />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import TheHeader from '../components/TheHeader.vue'
+import Tasks from '../components/Tasks.vue'
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: { TheHeader, Tasks },
 }
 </script>
+
+<style scoped>
+.home {
+  width: 100%;
+  height: 100%;
+}
+
+.tasks-header {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+}
+
+.tasks-header h1 {
+  margin-left: 3rem;
+}
+
+.select-tasks {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 3rem;
+  background-color: var(--blue-btn);
+  width: 150px;
+  height: 50px;
+  border-radius: 1rem;
+}
+
+.select-tasks h4 {
+  margin-right: 1rem;
+}
+</style>
